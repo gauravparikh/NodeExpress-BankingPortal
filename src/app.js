b9e3fe9d78fca939f9cb001134c901f000918362
gauravparikh <gauravparikh@users.noreply.github.com>
2018-08-12 18:47:02 -0700

    Completed the module

--- src/app.js
+++ src/app.js
@@ -10,4 +10,4 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res) => res.render('index',{title: 'Index'}));
 
-app.listen(3001, () => console.log("PS Project Running on port 3000"));
+app.listen(3000, () => console.log("PS Project Running on port 3000"));
