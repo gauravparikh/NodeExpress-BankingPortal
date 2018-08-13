e51032d57b9da9c356df9179cc97746fd5e741ca
gauravparikh <gauravparikh@users.noreply.github.com>
2018-08-12 19:35:56 -0700

    Completed the module

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const accountData = fs.readFileSync(
 const accounts = JSON.parse(accountData);
 
 const userData = fs.readFileSync(
-    path.JSON(__dirname,'json','users.json'),'utf8'
+    path.join(__dirname,'json','users.json'),'utf8'
 );
 
 const users = JSON.parse(userData);
